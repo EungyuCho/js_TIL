f76ded025f4a46cbc3a106a94884727e93ab6180
EungyuCho <cho2304@naver.com>
2020-05-13 13:08:34 +0900

    4.DOM-pig-game 주사위 구현 중간저장

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -8,3 +8,48 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+function user(name, score, current){
+    this.name = name;
+    this.score = score;
+    this.current = current;
+}
+
+const btn_new = document.getElementsByClassName('btn-new')[0];
+const btn_roll = document.getElementsByClassName('btn-roll')[0];
+const btn_hold = document.getElementsByClassName('btn-hold')[0];
+const score1 = document.getElementById('score-0');
+const score2 = document.getElementById('score-1');
+const current1 = document.getElementById('current-0');
+const current2 = document.getElementById('current-1');
+const dice = document.getElementById('dice');
+const imgArr = ['dice-1.png','dice-2.png','dice-3.png','dice-4.png','dice-5.png','dice-6.png'];
+let user1 = new user('user1', score1.innerText, current1.innerText);
+let user2 = new user('user2', score2.innerText, current2.innerHTML);
+
+btn_new.addEventListener('click', function(){
+    user1 = new user('user1', 0, 0);
+    user2 = new user('user2', 0, 0);
+
+    score1.innerText = 0;
+    score2.innerText = 0;
+
+    current1.innerText = 0;
+    current2.innerText = 0;
+
+    dice.style.display = 'none';
+    console.log(user1);
+    console.log(user2);
+});
+
+btn_roll.addEventListener('click',function(){
+    dice.style.display = 'block';
+
+    let diceValue = getRandomInt(1, 6);
+    dice.src = imgArr[diceValue-1];
+});
+
+function getRandomInt(min, max) { 
+    return Math.floor(Math.random() * (max - min + 1)) + min; 
+}
+
