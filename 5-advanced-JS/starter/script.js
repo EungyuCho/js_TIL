d7796b8b3d66d1dd13cba970dce4db3c30b9eee3
EungyuCho <cho2304@naver.com>
2020-05-16 23:15:10 +0900

    5.advanced-js : 원시타입과 객체의 차이

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -1,5 +1,7 @@
 // function constructor
+
 // 객체 생성
+/*
 let date = new Date();
 function hello(){
 
@@ -47,3 +49,46 @@ console.log(jane.lastName);
 
 //__proto__는 객체가 새로 생성 될 때 조상이었던 함수(Person)의 Prototype Object를 가르킨다!
 console.log(Object.getPrototypeOf(mark));
+
+*/
+
+// Primitive : call by value
+var a = 23;
+var b = a;
+a = 46;
+
+console.log(a);
+console.log(b);
+
+// Objects : call by reference
+var obj1 = {
+    name: 'john',
+    age : 26
+}
+var obj2 = obj1;
+obj2.age = 30;
+
+console.log(obj1.age);
+console.log(obj2.age);
+
+//Functions
+var age = 27;
+var obj = {
+    name : 'jonas',
+    city : 'risbon'
+};
+
+function change(a,b){
+    a = 30;
+    b.city = 'San Francisco'
+}
+
+change(age, obj);
+
+console.log(age);       //원시값이므로 함수에서 값을 전달받았을뿐이기때문에 a는 바뀌지않음
+console.log(obj.city);  //값에대한 주소를 넘겨주므로 데이터가 변경되었다!!
+
+
+/////////////////////////////
+// Lecture: Passing functions as arguments
+/*
