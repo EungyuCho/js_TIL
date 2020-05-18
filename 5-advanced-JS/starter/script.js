122a053a5e7095091d4d716671b388b946fa18f0
unknown <cho2304@naver.com>
2020-05-18 21:35:57 +0900

    5.advanced-js : 클로저

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -181,6 +181,9 @@ function game(){
 }
 game();
 */
+// 데이터가 안전함(즉시 실행이므로 데이터가 노출되지 않음.)
+
+/*
 (function(){
     var score = Math.random() * 10;
     console.log(score >= 5);
@@ -190,3 +193,68 @@ game();
     var score = Math.random() * 10;
     console.log(score >= 5 - goodLuck);
     })(5);
+
+*/
+/////////////////////////////////
+// Lecture : Closures
+// 클로저는 함수의 선언 위치에 따라서 결정되는 렉시컬 스코핑에서 외부함수가 내부함수보다 더 오래
+// 유지되는 경우 외부함수 밖에서 내부함수가 호출 되더라도 외부함수의 지역변수(a)에 접근할 수 있다.
+function retirement(retirementAge){
+    var a = ' years left until retirement.';
+    return function(yearOfBirth){
+        var age = new Date().getFullYear() - yearOfBirth;
+        console.log((retirementAge - age) + a);     //함수 스코프 밖에 a가 있지만 참조할 수 있음
+    }
+}
+
+var retirementUS = retirement(66);
+var retirementGermany = retirement(65);
+var retirementIceland = retirement(67);
+
+retirementGermany(1990);
+retirementUS(1990);
+retirementIceland(1990);
+
+/*
+function interviewQuestion(job) {
+    if (job === 'designer') {
+        return function(name) {
+            console.log(name + ', can you please explain what UX design is?');
+        }
+    } else if (job === 'teacher') {
+        return function(name) {
+            console.log('What subject do you teach, ' + name + '?');
+        }
+    } else {
+        return function(name) {
+            console.log('Hello ' + name + ', what do you do?');
+        }
+    }
+}
+*/
+
+
+function interviewQuestion(job){
+    return function(name){
+        switch(job){
+            case 'designer':
+                console.log(name + ', can you please explain what UX design is?');
+                break;
+            case 'teacher':
+                console.log('What subject do you teach, ' + name + '?');
+                break;
+            default:
+                console.log('Hello ' + name + ', what do you do?');
+                break;
+        }
+    }
+}
+
+
+var design = interviewQuestion('designer');
+var teacher = interviewQuestion('teacher');
+var programmer = interviewQuestion('programmer');
+
+design('mina');
+teacher('minsu');
+programmer('robot');
