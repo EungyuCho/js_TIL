65a4a26b5ab307e5fbdd7705fb38e2ab1c94049f
unknown <cho2304@naver.com>
2020-05-23 21:18:20 +0900

    advancedJS challenge complete

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -353,70 +353,148 @@ c) correct answer (I would use a number for this)
 */
 
 
+// (function(){
+//     var score = 0;
+//     function Question(question, answers, correct) {
+//         this.score = score;
+//         this.question = question;
+//         this.answers = answers;
+//         this.correct = correct;
+//     }
+    
+//     Question.prototype.displayQuestion = function () {
+//         console.log(this.question);
+
+//         for (var i = 0; i < this.answers.length; i++) {
+//             console.log(i + ': ' + this.answers[i]);
+//         }
+//     }
+
+//     Question.prototype.displayScore = function () {
+//         console.log('Your current score is: ' + score);
+//         console.log('=================================');
+//     }
+//     Question.prototype.checkAnswer = function(ans) {
+//         console.log('ans는 '+ans);
+//         if (ans === this.correct) {
+//             console.log('Correct answer!');
+//             score += 2;
+//             gameStart();
+//         } else if(ans === undefined){
+//             console.log('game end!');    
+//         }else {
+//             console.log('Wrong answer. Try again :)');
+//             gameStart();
+
+//         }
+//     }
+
+//     var q1 = new Question('Is JavaScript the coolest programming language in the world?',
+//                           ['Yes', 'No'],
+//                           0);
+
+//     var q2 = new Question('What is the name of this course\'s teacher?',
+//                           ['John', 'Micheal', 'Jonas'],
+//                           2);
+
+//     var q3 = new Question('What does best describe coding?',
+//                           ['Boring', 'Hard', 'Fun', 'Tediuos'],
+//                           2);
+
+//     var questions = [q1, q2, q3];
+
+//     var n;
+
+//     var gameStart = function(){
+//         n = Math.floor(Math.random() * questions.length);
+    
+//         questions[n].displayQuestion();
+    
+//         var answer = parseInt(prompt('Please select the correct answer.'));
+    
+//         if(answer !== 'exit'){
+//             questions[n].checkAnswer(answer);
+//             gameStart();
+//         }
+//     }
+
+//     gameStart();
+// })();
+
 (function(){
-    var score = 0;
-    function Question(question, answers, correct) {
-        this.score = score;
+    function Question(question, answer, currect){
         this.question = question;
-        this.answers = answers;
-        this.correct = correct;
-    }
-    
-    Question.prototype.displayQuestion = function () {
-        console.log(this.question);
-
-        for (var i = 0; i < this.answers.length; i++) {
-            console.log(i + ': ' + this.answers[i]);
-        }
+        this.answer = answer;
+        this.currect = currect;
     }
 
-    Question.prototype.displayScore = function () {
-        console.log('Your current score is: ' + score);
-        console.log('=================================');
+    Question.prototype.displayQuestion = function(){
+        console.log(this.question)
+        let count = 1;
+        this.answer.forEach(element => {
+            console.log(count + '. ' + element)
+            count++;
+        });
+
     }
-    Question.prototype.checkAnswer = function(ans) {
-        console.log('ans는 '+ans);
-        if (ans === this.correct) {
+
+    Question.prototype.checkAnswer = function(ans, callback){
+        let sc;
+
+        if(ans === this.currect){
             console.log('Correct answer!');
-            score += 2;
-            gameStart();
-        } else if(ans === undefined){
-            console.log('game end!');    
-        }else {
-            console.log('Wrong answer. Try again :)');
-            gameStart();
+            sc = callback(true);
+        }else{
+            console.log('Wrong answer! try again!');
+            sc = callback(false);
+        }
 
+        this.displayScore(sc);
+
+    }
+
+    Question.prototype.displayScore = function(score){
+        console.log('Your current socre is :' + score)
+    }
+
+    function score(){
+        let sc = 0;
+        return function(correct){
+            if(correct){
+                sc++;
+            }
+            return sc;
         }
     }
 
-    var q1 = new Question('Is JavaScript the coolest programming language in the world?',
-                          ['Yes', 'No'],
-                          0);
+    const keepScore = score();
 
-    var q2 = new Question('What is the name of this course\'s teacher?',
-                          ['John', 'Micheal', 'Jonas'],
-                          2);
+    const q1 = new Question('Is JavaScript the coolest programming Language in world?'
+                                , ['Yes', 'No'], 0);
+                                
+    const q2 = new Question('What is the name of this course\'s teacher?'
+    , ['John', 'Michael', 'Jonas'], 2);
 
-    var q3 = new Question('What does best describe coding?',
-                          ['Boring', 'Hard', 'Fun', 'Tediuos'],
-                          2);
+    const questions = [q1, q2];
 
-    var questions = [q1, q2, q3];
+    const gameStart = function(){
+        const questionIndex = Math.floor(Math.random() * questions.length);
+        
+        const targetQuestion = questions[questionIndex];
 
-    var n;
+        targetQuestion.displayQuestion()
+        
+        let ans =  prompt('Please select correct answer');
 
-    var gameStart = function(){
-        n = Math.floor(Math.random() * questions.length);
-    
-        questions[n].displayQuestion();
-    
-        var answer = parseInt(prompt('Please select the correct answer.'));
-    
-        if(answer !== 'exit'){
-            questions[n].checkAnswer(answer);
+        console.log(ans);
+
+        if(ans !== 'exit'){
+            targetQuestion.checkAnswer(parseInt(ans), keepScore);
+            
             gameStart();
         }
     }
 
     gameStart();
+
 })();
