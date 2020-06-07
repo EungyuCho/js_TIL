2c36446b688b7c334967a04d37e6bd30cb4e0e97
unknown <cho2304@naver.com>
2020-06-07 23:29:53 +0900

    '7.ES6:arrowbasicfrunction2'

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -25,3 +25,82 @@ ages6 = years.map((el, index) => {
 });
 
 console.log(ages6);
+
+//Arrow functions2
+
+//ES5
+
+var box5 = {
+    color: 'green',
+    position: 1,
+    clickMe: function () {
+        var self = this;
+        document.querySelector('.green' +
+            '').addEventListener('click', function () {
+            var str = 'This is box number ' + self.position +
+                ' and it is ' + self.color;
+            alert(str);
+        });
+    }
+}
+
+// box5.clickMe();
+
+
+const box6 = {
+    color: 'green',
+    position: 1,
+    clickMe: function () {
+        document.querySelector('.green' +
+            '').addEventListener('click', () => {
+            var str = 'This is box number ' + this.position +
+                ' and it is ' + this.color;
+            alert(str);
+        });
+    }
+}
+
+box6.clickMe();
+
+/*
+const box66 = {
+    color: 'green',
+    position: 1,
+    clickMe: () => {
+        document.querySelector('.green' +
+            '').addEventListener('click', () => {
+            var str = 'This is box number ' + this.position +
+                ' and it is ' + this.color;
+            alert(str);
+        });
+    }
+}
+
+box66.clickMe();
+*/
+
+
+function Person(name) {
+    this.name = name;
+}
+
+//ES5
+Person.prototype.myFriends5 = function (friend) {
+    var arr = friend.map(function (el) {
+       return this.name + ' is friends with ' + el;
+    }.bind(this));
+    console.log(arr)
+};
+
+var friends = ['Bob', 'Jane', 'Makr'];
+
+new Person('John').myFriends5(friends);
+
+//ES6
+
+Person.prototype.myFriends6 = function (friend) {
+    const arr = friend.map(el => `${this.name} is friends with ${el}`);
+    console.log(arr)
+};
+
+new Person('John').myFriends6(friends);
