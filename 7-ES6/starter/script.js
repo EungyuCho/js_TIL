f7c2e0c247dbd3514b61118f4e030bbcc98e595b
unknown <cho2304@naver.com>
2020-06-07 23:02:11 +0900

    7. ES6 : Arrow basic function

---
+++ 7-ES6/starter/script.js
@@ -0,0 +1,27 @@
+// Arrow functions
+
+const years = [1995, 1965, 1982, 1937]
+
+//ES5
+var ages5 = years.map(function (el) {
+    return 2020 - el;
+});
+
+console.log(ages5);
+
+//ES6
+
+let ages6 = years.map(el => 2020 - el);
+console.log(ages6);
+
+ages6 = years.map((el, index) => `Age element ${index + 1}: ${2016 - el}.`);
+console.log(ages6);
+
+ages6 = years.map((el, index) => {
+    const now = new
+    Date().getFullYear();
+    const age = now - el;
+    return `Age element ${index + 1}:${age}`
+});
+
+console.log(ages6);
