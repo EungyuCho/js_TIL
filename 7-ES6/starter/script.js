8b4d1b0a3832ac09bf79c2d5fd0f192b8323da56
gyu <cho2304@naver.com>
2020-06-09 22:09:48 +0900

    7:ES6 : The Spread Operator

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -87,7 +87,7 @@ function Person(name) {
 //ES5
 Person.prototype.myFriends5 = function (friend) {
     var arr = friend.map(function (el) {
-       return this.name + ' is friends with ' + el;
+        return this.name + ' is friends with ' + el;
     }.bind(this));
     console.log(arr)
 };
@@ -143,3 +143,38 @@ const [age, retirement] = calcAgeRetirement(1990);
 
 console.log(age)
 console.log(retirement)
+
+
+//Spread operator
+
+function addFourAges(a, b, c, d) {
+    return a + b + c + d;
+}
+
+var sum1 = addFourAges(18, 30, 12, 21);
+console.log(sum1);
+
+
+//ES5
+var ages = [18, 30, 12,  21];
+var sum2 = addFourAges.apply(null, ages);
+console.log(sum2)
+
+//ES6
+const max3 = addFourAges(...ages);
+console.log(max3);
+
+const familySmith = ['John', 'Jane', 'Mark'];
+const familyMiller = ['Mary', 'Bob', 'Ann'];
+const bigFamily = [...familyMiller, ...familySmith];
+console.log(bigFamily)
+
+const h = document.querySelector('h1');
+const boxes = document.querySelectorAll('.box');
+
+console.log(h);
+console.log(boxes);
+
+const all = [h, ...boxes];
+Array.from(all).forEach(cur => cur.style.color = 'purple')
+
