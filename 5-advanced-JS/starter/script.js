08701a9d2e8af911c09f149e3fb8b3217d6218c5
gyu <cho2304@naver.com>
2020-11-21 16:48:44 +0900

    5. Create Objects: Passing functions as arguments & Functions returning functions

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -30,7 +30,7 @@ console.log(john.lastName);
 console.log(jane.lastName);
 */
 
-
+/*
 // Primitives vs objects
 
 // Primitives
@@ -69,3 +69,68 @@ change(age, obj);
 
 console.log(age);
 console.log(obj.city);
+*/
+
+/*
+///////////////////////////////
+// Lectures : Passing functions as arguments
+
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+    var arrResult = [];
+    for (var i = 0; i < arr.length; i++) {
+        arrResult.push(fn(arr[i]));
+    }
+
+    return arrResult;
+}
+
+function calculateAge(el) {
+    return 2020 - el;
+}
+
+function isFullAge(el) {
+    return el >= 18;
+}
+
+function maxHeartRate(el) {
+    if (el >= 18 && el <= 81)
+        return Math.round(206.9 - (0.67 * el));
+
+    return -1;
+}
+
+var ages = arrayCalc(years, calculateAge);
+var fullAges = arrayCalc(ages, isFullAge);
+var rates = arrayCalc(ages, maxHeartRate);
+console.log(ages);
+console.log(fullAges);
+console.log(rates);
+*/
+
+///////////////////////////////////////
+//// Lecture : Functions returning functions
+
+function interviewQuestion(job) {
+    if (job === 'designer') {
+        return (name) => {
+            console.log(name + ', can you please explain what UX design is?');
+        }
+    } else if (job === 'teacher') {
+        return (name) => {
+            console.log('What subject do you teach, ' + name + '?');
+        };
+    } else {
+        return (name) => {
+            console.log('Hello ' + name + ', what do you do?');
+        }
+    }
+}
+
+var teacherQuestion = interviewQuestion('teacher');
+var designerQuestion = interviewQuestion('designer');
+teacherQuestion('John');
+designerQuestion('jane');
+designerQuestion('Mark');
+interviewQuestion('teacher')('Mike');
